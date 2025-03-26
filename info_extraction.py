b84c37f9c9216cf2f156798b4efbf0ae0a8065bc
Oct-autumn <octautumn2002@gmail.com>
2025-03-26 23:27:49 +0800

    refactor: 优化信息提取流程，增加临时结果缓存和无效实体过滤

--- info_extraction.py
+++ info_extraction.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 
 import tqdm
 
@@ -8,6 +10,8 @@ from src.ie_process import info_extract_from_str
 from src.llm_client import LLMClient
 from src.open_ie import OpenIE
 from src.raw_processing import load_raw_data
+
+TEMP_DIR = "./temp"
 
 
 def main():
@@ -25,35 +29,44 @@ def main():
     sha256_list, raw_datas = load_raw_data()
     logger.info("原始数据加载完成\n")
 
-    entity_lists = []
-    rdf_triple_lists = []
-    failed_sha256 = []
+    # 创建临时目录
+    if not os.path.exists(f"{TEMP_DIR}"):
+        os.makedirs(f"{TEMP_DIR}")
 
-    for sha, raw_data in tqdm.tqdm(zip(sha256_list, raw_datas), postfix="正在进行提取：", total=len(sha256_list)):
+    failed_sha256 = []
+    open_ie_doc = []
+    for pg_hash, raw_data in tqdm.tqdm(
+        zip(sha256_list, raw_datas), postfix="正在进行提取："
+    ):
+        if os.path.exists(f"{TEMP_DIR}/{pg_hash}.json"):
+            # 存在对应的提取结果
+            logger.info(f"找到缓存的提取结果：{pg_hash}")
+            with open(f"{TEMP_DIR}/{pg_hash}.json", "r", encoding="utf-8") as f:
+                open_ie_doc.append(json.load(f))
+            continue
+
         entity_list, rdf_triple_list = info_extract_from_str(
             llm_client_list[global_config["entity_extract"]["llm"]["provider"]],
             llm_client_list[global_config["rdf_build"]["llm"]["provider"]],
             raw_data,
         )
         if entity_list is None or rdf_triple_list is None:
-            failed_sha256.append(sha)
-            logger.error(f"提取失败：{sha}")
+            failed_sha256.append(pg_hash)
+            logger.error(f"提取失败：{pg_hash}")
             continue
         else:
-            entity_lists.append(entity_list)
-            rdf_triple_lists.append(rdf_triple_list)
-
-    open_ie_doc = []
-    for pg_hash, raw_data, entity_list, rdf_triple_list in zip(sha256_list, raw_datas, entity_lists, rdf_triple_lists):
-        open_ie_doc.append(
-            {
+            doc_item = {
                 "idx": pg_hash,
                 "passage": raw_data,
                 "extracted_entities": entity_list,
                 "extracted_triples": rdf_triple_list,
             }
-        )
+            open_ie_doc.append(doc_item)
+            # 保存临时提取结果
+            with open(f"{TEMP_DIR}/{pg_hash}.json", "w", encoding="utf-8") as f:
+                f.write(json.dumps(doc_item, ensure_ascii=False, indent=4))
 
+    # 保存信息提取结果
     sum_phrase_chars = sum(
         [len(e) for chunk in open_ie_doc for e in chunk["extracted_entities"]]
     )
@@ -61,13 +74,11 @@ def main():
         [len(e.split()) for chunk in open_ie_doc for e in chunk["extracted_entities"]]
     )
     num_phrases = sum([len(chunk["extracted_entities"]) for chunk in open_ie_doc])
-
     openie_obj = OpenIE(
         open_ie_doc,
         round(sum_phrase_chars / num_phrases, 4),
         round(sum_phrase_words / num_phrases, 4),
     )
-
     OpenIE.save(openie_obj)
 
     logger.info("--------信息提取完成--------")
